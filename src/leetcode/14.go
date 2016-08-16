80e1adfe5eecba60f6ceb6b56bcd0fb61bb02903
PasseRR <xie__hai@sina.com>
2016-08-16 14:11:41 +0800

    Longest Common Prefix

---
+++ src/leetcode/14.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	if len(strs) == 1 {
+		return strs[0]
+	}
+	min := math.MaxInt32
+	var shortest string
+	for _, value := range strs {
+		if len(value) < min {
+			min = len(value)
+			shortest = value
+		}
+	}
+
+	if len(shortest) == 0 {
+		return shortest
+	}
+
+	for index, _ := range shortest {
+		for _, str := range strs {
+			if shortest[0:index+1] != string(str)[0:index+1] {
+				return shortest[0:index]
+			}
+		}
+	}
+
+	return shortest
+}
+
+func main() {
+	fmt.Printf("%v", longestCommonPrefix([]string{"aab", "aac"}))
+}
