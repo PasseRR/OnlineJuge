d3d46ec7c5a121bfb3e9ba009e68751511f2664d
PasseRR <xie__hai@sina.com>
2016-08-16 17:02:28 +0800

    Valid Parentheses

---
+++ src/leetcode/20.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	parentheses := []string{}
+	for _, value := range s {
+		parenthesis := string(value)
+		if parenthesis == "(" ||
+			parenthesis == "[" ||
+			parenthesis == "{" {
+			// 开括号入栈
+			parentheses = append(parentheses, parenthesis)
+		} else {
+			len := len(parentheses)
+			// 括号是否是开关对应
+			if len < 1 ||
+				(parenthesis == ")" && parentheses[len-1] != "(") ||
+				(parenthesis == "]" && parentheses[len-1] != "[") ||
+				(parenthesis == "}" && parentheses[len-1] != "{") {
+				return false
+			} else {
+				// 开括号出栈
+				parentheses = parentheses[0 : len-1]
+			}
+		}
+	}
+
+	return len(parentheses) == 0
+}
+
+func main() {
+	fmt.Printf("%v", isValid("((()))"))
+}
