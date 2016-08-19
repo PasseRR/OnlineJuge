8e625822d0160699d1d15e51196a0b63571a7a5b
PasseRR <xie__hai@sina.com>
2016-08-19 11:06:01 +0800

    Reverse Nodes in k-Group

---
+++ src/leetcode/25_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func TestReverseKGroup(t *testing.T) {
+	node := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val: 6,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	printNode(reverseKGroup(node, 2))
+	printNode(reverseKGroup(node, 4))
+}
