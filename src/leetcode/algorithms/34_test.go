845eabdb666150ab8382335ee5af46fe44848466
PasseRR <xie__hai@sina.com>
2016-11-03 11:53:08 +0800

    34. Search for a Range

---
+++ src/leetcode/algorithms/34_test.go
@@ -0,0 +1,12 @@
+package algorithms
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSearchRange(t *testing.T) {
+	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 9}, 8))
+	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 9}, 7))
+	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 9}, 9))
+}
