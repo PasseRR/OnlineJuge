d9b9f2cca6105105b5cb064d4dee595591d40278
PasseRR <xie__hai@sina.com>
2018-01-12 17:06:07 +0800

    180. Consecutive Numbers

--- src/leetcode/database/180.sql
+++ src/leetcode/database/180.sql
@@ -1,3 +1,16 @@
+-- 方法一
+SELECT DISTINCT l1.Num AS ConsecutiveNums
+FROM
+  Logs l1,
+  Logs l2,
+  Logs l3
+WHERE
+  l1.Id = l2.Id - 1
+  AND l2.Id = l3.Id - 1
+  AND l1.Num = l2.Num
+  AND l2.Num = l3.Num;
+
+-- 方法二
 SELECT DISTINCT Num AS ConsecutiveNums
 FROM (
        SELECT
@@ -11,4 +24,4 @@ FROM (
        FROM Logs, (SELECT @prev := NULL) X
        ORDER BY Id
      ) AS A
-WHERE A.CNT >= 3
+WHERE A.CNT >= 3;
