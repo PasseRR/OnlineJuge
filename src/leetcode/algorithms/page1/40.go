2f7f7e8f7d6fbded44ee281fa6cddcc75ff3bb7b
PasseRR <xie__hai@sina.com>
2018-01-16 16:23:03 +0800

    40. Combination Sum II

---
+++ src/leetcode/algorithms/page1/40.go
@@ -0,0 +1,71 @@
+package page1
+
+import (
+	"sort"
+)
+
+var combination2 [][]int
+
+func combinationSum2(candidates []int, target int) [][]int {
+	// 最终结果
+	result := [][]int{}
+	// 初始化结果集
+	combination2 = [][]int{}
+	sort.Ints(candidates)
+	backtrackingSum2(candidates, []int{}, target)
+	// 去重
+	for _, sub := range combination2 {
+		sort.Ints(sub)
+		if !isSolutionExists(sub, result) {
+			result = append(result, sub)
+		}
+	}
+	return result
+}
+
+func isSolutionExists(solution []int, result [][]int) bool {
+	for _, sub := range result {
+		if len(sub) != len(solution) {
+			continue
+		}
+
+		flag := true
+		for j, value := range sub {
+			if solution[j] != value {
+				flag = false
+				break
+			}
+		}
+
+		if flag {
+			return flag
+		}
+	}
+
+	return false
+}
+
+func backtrackingSum2(candidates, solution []int, target int) {
+	for i, candidate := range candidates {
+		if candidate < target {
+			solution = append(solution, candidate)
+			// 每个元素只能使用一次 复制candidates并移除当前元素
+			c := make([]int, len(candidates))
+			copy(c, candidates)
+			// 减去已经加上的数字
+			backtrackingSum2(append(c[:i], c[i+1:]...), solution, target-candidate)
+			// 回溯
+			solution = solution[:len(solution)-1]
+		} else {
+			if candidate == target {
+				solution = append(solution, candidate)
+				// 复制slice
+				s := make([]int, len(solution))
+				copy(s, solution)
+				combination2 = append(combination2, s)
+			}
+
+			break
+		}
+	}
+}
---
+++ src/leetcode/algorithms/page1/40_test.go
@@ -0,0 +1,10 @@
+package page1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCombinationSum2(t *testing.T) {
+	fmt.Printf("%v\n", combinationSum2([]int{10, 1, 2, 7, 6, 1, 5}, 8))
+}
