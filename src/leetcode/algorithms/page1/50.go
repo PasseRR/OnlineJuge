014eaa7b017a93af35081c417878087cb1622a8a
PasseRR <xie__hai@sina.com>
2018-01-18 15:04:24 +0800

    50. Pow(x, n)

--- src/leetcode/algorithms/page1/50.go
+++ src/leetcode/algorithms/page1/50.go
@@ -1,21 +1,21 @@
 package page1
 
 func myPow(x float64, n int) float64 {
-	result, isNegative := float64(1), false
+	result := float64(1)
 
+	// 负n次方
 	if n < 0 {
-		isNegative, n = true, -n
+		x, n = 1/x, -n
 	}
 
+	// x^2n = x^n * x^n
+	// x^(2n + 1) = x^n * x^n * x
+	// 2^10 = 2^4 * 2^4 * 2 * 2
 	for i := n; i != 0; i >>= 1 {
 		if i%2 != 0 {
 			result *= x
 		}
 		x *= x
-	}
-
-	if isNegative {
-		result = 1 / result
 	}
 
 	return result
